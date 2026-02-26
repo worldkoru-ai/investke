f74ff80668fc8c579e2af7f6fa8449d3c3d3ab20
stellah660 <valestellah165@gmail.com>
2026-02-26 10:45:59 +0300

    verify

--- src/app/api/admin/users/route.ts
+++ src/app/api/admin/users/route.ts
@@ -10,7 +10,7 @@ export async function GET(req: NextRequest) {
     const db = getDb();
     const [users]: any = await db.query(`
       SELECT 
-        id, name, email, walletBalance, totalInvested, totalInterestEarned, created_at AS createdAt,
+        id, name, email, walletBalance, totalInvested, totalInterestEarned, created_at AS createdAt, isVerified,
         isAdmin
       FROM users
     `);
